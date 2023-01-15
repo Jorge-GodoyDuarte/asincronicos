dc375937fc545a6d463ce47bbcfa3749b6be6860
Jorge-GodoyDuarte <hawkaa666@gmail.com>
2023-01-15 22:28:39 +0100

    tp finalizado

---
+++ public/js/favoritas.js
@@ -0,0 +1,96 @@
+window.onload = () => {
+  const app = document.getElementById("root");
+  const container = document.createElement("div");
+  container.setAttribute("class", "container");
+  app.appendChild(container);
+
+ 
+  fetch("http://localhost:3001/api/movies")
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      let data2 = data.data;
+      let favoritas = localStorage.getItem('favorite')
+      
+      if (JSON.parse(favoritas).length === 0) {
+        document.getElementById("containerPelisFavs").innerHTML = "no hay pelis favs pa, toca la estrellita de alguna movie que te guste crack"
+        document.getElementById("containerPelisFavs").style.fontSize = "40px"
+      }
+      data2.forEach((movie) => {
+        if (favoritas.includes(movie.id)) {
+          const card = document.createElement("div");
+          card.setAttribute("class", "card");
+
+          const h1 = document.createElement("h1");
+          h1.textContent = movie.title;
+          if (favoritas.includes(movie.id)) {
+            h1.innerHTML += `<br><i class='fa-solid fa-star' id='${movie.id}'></i>`
+          } else {
+            h1.innerHTML += `<br><i class='fa-regular fa-star' id='${movie.id}'></i>`
+          }
+
+
+          const p = document.createElement("p");
+          p.textContent = `Rating: ${movie.rating}`;
+
+          const duracion = document.createElement("p");
+          duracion.textContent = `Duración: ${movie.length}`;
+
+          const boton = document.createElement("button");
+          boton.innerHTML = `<a href='http://localhost:3001/movies/detail/${movie.id}'> Editar </a>`;
+
+          container.appendChild(card);
+
+          card.appendChild(h1);
+
+          card.appendChild(p);
+          if (movie.genre !== null) {
+            const genero = document.createElement("p");
+            genero.textContent = `Genero: ${movie.genre.name}`;
+            card.appendChild(genero);
+          }
+          card.appendChild(duracion);
+
+          card.appendChild(boton);
+
+          let estrellita = document.getElementById(`${movie.id}`)
+          estrellita.addEventListener("click", (e) => {
+            let id = e.target.id
+            let carListFav = [id]
+
+            let recoveredData = localStorage.getItem('favorite')
+
+            if (recoveredData == null) {
+              localStorage.setItem('favorite', JSON.stringify(carListFav))
+            } else {
+              let data = JSON.parse(recoveredData)
+              let newCar = id
+
+              if (!data.includes(newCar)) {
+                data.push(newCar)
+                estrellita.classList.remove("fa-regular")
+                estrellita.classList.add("fa-solid")
+                localStorage.setItem('favorite', JSON.stringify(data))
+              } else {
+                let indice = data.indexOf(newCar);
+                data.splice(indice, 1);
+
+                estrellita.classList.remove("fa-solid")
+                estrellita.classList.add("fa-regular")
+                localStorage.setItem('favorite', JSON.stringify(data))
+              }
+            };
+          })
+        }
+        
+      });
+     
+    })
+    .catch(function (error) {
+      console.log(error)
+    })
+
+
+ 
+};
